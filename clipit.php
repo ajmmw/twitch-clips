21da8c743a24700bc30e5d5f5376567cb000c61e
PnKllr <pnkllr@gmail.com>
2022-06-17 00:03:58 +1000

    Update clipit.php

--- clipit.php
+++ clipit.php
@@ -1,18 +1,21 @@
 <?php
-/* The ID is your twitch user_id NOT your channels username
-   The TOKEN is your generated auth token used to created clips on your behalf see: https://twitchtokengenerator.com */
-   
-	$id = '';
-	$token = '';
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, 'https://api.twitch.tv/helix/clips?broadcaster_id='.$id);
-	curl_setopt( $ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-	curl_setopt($ch, CURLOPT_HTTPHEADER, ['Authorization: Bearer ' . $token]);
-	$output   = curl_exec($ch);
-	$response = json_decode($output);
-	curl_close($ch);
-	if (!empty($response->data)) {
-		echo 'Clip created: http://clips.twitch.tv/'.$response->data[0]->id;
-	} 
+
+$id = '';
+$client_id = '';
+$access_tokrn = '';
+
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'https://api.twitch.tv/helix/clips?broadcaster_id=' . $id);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['Client-ID: ' . $client_id, 'Authorization: Bearer ' . $access_token]);
+$output   = curl_exec($ch);
+$response = json_decode($output);
+curl_close($ch);
+if (!empty($response->data)) {
+	$clipurl = 'http://clips.twitch.tv/' . $response->data[0]->id;
+	sleep(5);
+	echo $clipurl
+}
